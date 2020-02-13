a508bc8853f3a23473eaf4876dd25103e8660952
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-13 16:17:01 +0100

    create model for user and validate data for registration

---
+++ models/User.js
@@ -0,0 +1,68 @@
+const validator = require('validator');
+
+let User = function(data) {
+  this.data = data;
+  this.errors = [];
+};
+
+User.prototype.cleanUp = function() {
+  // accept only strings
+  if (typeof this.data.username != 'string') {
+    this.data.username = '';
+  }
+  if (typeof this.data.email != 'string') {
+    this.data.email = '';
+  }
+  if (typeof this.data.password != 'string') {
+    this.data.password = '';
+  }
+
+  // accept only defined properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password
+  };
+};
+
+User.prototype.validate = function() {
+  if (this.data.username == '') {
+    this.errors.push('You must provide a username');
+  }
+  if (
+    this.data.username != '' &&
+    !validator.isAlphanumeric(this.data.username)
+  ) {
+    this.errors.push('Username can only contain letters and numbers');
+  }
+  if (!validator.isEmail(this.data.email)) {
+    this.errors.push('You must provide a valid email address');
+  }
+  if (this.data.password == '') {
+    this.errors.push('You must provide a password');
+  }
+
+  if (this.data.username.length > 0 && this.data.username.length < 3) {
+    this.errors.push('Username must be at least 3 characters');
+  }
+  if (this.data.password.length > 0 && this.data.password.length < 12) {
+    this.errors.push('Password must be at least 12 characters');
+  }
+
+  if (this.data.username.length > 30) {
+    this.errors.push('Username must not exceed 30 characters');
+  }
+  if (this.data.password.length > 100) {
+    this.errors.push('Password must not exceed 100 characters');
+  }
+};
+
+User.prototype.register = function() {
+  // step #1: validate user data
+  this.cleanUp();
+  this.validate();
+
+  // step #2: if there are no validation errors then store user data in database
+};
+
+module.exports = User;
