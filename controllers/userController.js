08ee4363a0f0b8d57201eb1c307a47fc7b22291b
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-16 18:00:09 +0100

    working on create-post: reusable function => only logged in users may create posts

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/User');
 
+exports.mustBeLoggedIn = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash('errors', 'You must be logged in to perform that action');
+    req.session.save(callback => res.redirect('/'));
+  }
+};
+
 exports.login = function(req, res) {
   let user = new User(req.body);
   user
