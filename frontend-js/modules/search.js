d0b49ad6aab91eba5d7c470080b605bec841e033
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-21 15:48:13 +0100

    Working on search: show static example results if http request finds posts otherwise show no results message

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -44,12 +44,50 @@ export default class Search {
       .post('/search', { searchTerm: this.inputField.value })
       .then(response => {
         console.log(response.data);
+        this.renderResultsHTML(response.data);
       })
       .catch(err => alert('An error occurred on sending request'));
   }
 
+  renderResultsHTML(posts) {
+    if (posts.length) {
+      this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+      <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
+
+      <a href="#" class="list-group-item list-group-item-action">
+        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
+        <span class="text-muted small">by barksalot on 0/14/2019</span>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">
+        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
+        <span class="text-muted small">by brad on 0/12/2019</span>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">
+        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
+        <span class="text-muted small">by barksalot on 0/14/2019</span>
+      </a>
+      <a href="#" class="list-group-item list-group-item-action">
+        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
+        <span class="text-muted small">by brad on 0/12/2019</span>
+      </a>
+    </div>`;
+    } else {
+      this.resultsArea.innerHTML = `<p>Sorry, but we could not find any results for your search</p>`;
+    }
+    this.hideLoaderIcon();
+    this.showResultsArea();
+  }
+
   showLoaderIcon() {
     this.loaderIcon.classList.add('circle-loader--visible');
+  }
+
+  hideLoaderIcon() {
+    this.loaderIcon.classList.remove('circle-loader--visible');
+  }
+
+  showResultsArea() {
+    this.resultsArea.classList.add('live-search-results--visible');
   }
 
   openOverlay() {
@@ -76,28 +114,7 @@ export default class Search {
     <div class="search-overlay-bottom">
       <div class="container container--narrow py-3">
         <div class="circle-loader"></div>
-        <div class="live-search-results">
-          <div class="list-group shadow-sm">
-            <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-              <span class="text-muted small">by barksalot on 0/14/2019</span>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-              <span class="text-muted small">by brad on 0/12/2019</span>
-            </a>
-          </div>
-        </div>
+        <div class="live-search-results"></div>
       </div>
     </div>
   </div>`
