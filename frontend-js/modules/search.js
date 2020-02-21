925c596f6f9ef3e612d4a3bd2e3364d9d58d31a0
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-21 18:46:24 +0100

    Working on search: dompurify protects against malicious code

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import DOMPurify from 'dompurify';
 
 export default class Search {
   // 1. Select DOM Elements, and keep track of any useful data
@@ -63,7 +64,7 @@ export default class Search {
 
   renderResultsHTML(posts) {
     if (posts.length) {
-      this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
+      this.resultsArea.innerHTML = DOMPurify.sanitize(`<div class="list-group shadow-sm">
       <div class="list-group-item active"><strong>Search Results</strong> 
       (${
         posts.length > 1 ? `${posts.length} items found` : '1 item found'
@@ -83,7 +84,7 @@ export default class Search {
             </a>`;
         })
         .join('')}
-    </div>`;
+    </div>`);
     } else {
       this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, but we could not find any results for your search</p>`;
     }
