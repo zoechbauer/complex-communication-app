855d66bfed46dad3fc5aedd2e51ae5fbd9bc4365
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-21 17:19:46 +0100

    Working on search: display http results data instead of faked example data

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -64,27 +64,28 @@ export default class Search {
   renderResultsHTML(posts) {
     if (posts.length) {
       this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-      <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
+      <div class="list-group-item active"><strong>Search Results</strong> 
+      (${
+        posts.length > 1 ? `${posts.length} items found` : '1 item found'
+      })</div>
 
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-        <span class="text-muted small">by barksalot on 0/14/2019</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-        <span class="text-muted small">by brad on 0/12/2019</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-        <span class="text-muted small">by barksalot on 0/14/2019</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-        <span class="text-muted small">by brad on 0/12/2019</span>
-      </a>
+      ${posts
+        .map(post => {
+          let postDate = new Date(post.createdDate);
+          return `<a href="/post/${
+            post._id
+          }" class="list-group-item list-group-item-action">
+            <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${
+            post.title
+          }</strong>
+            <span class="text-muted small">by ${post.author.username}
+            on ${postDate.toLocaleDateString()} ${postDate.toLocaleTimeString()}</span>
+            </a>`;
+        })
+        .join('')}
     </div>`;
     } else {
-      this.resultsArea.innerHTML = `<p>Sorry, but we could not find any results for your search</p>`;
+      this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, but we could not find any results for your search</p>`;
     }
     this.hideLoaderIcon();
     this.showResultsArea();
