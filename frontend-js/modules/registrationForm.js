07cad28de5641da778ae7bc502a437d88d1d2f7d
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-26 13:11:16 +0100

    Working on validation: alert message if entered username has changed - use generic function

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -4,18 +4,35 @@ export default class RegistrationForm {
       '#registration-form .form-control'
     );
     this.insertValidationElements();
+    this.username = document.querySelector('#username-register');
+    this.username.previousValue = '';
     this.events();
   }
 
   //events
-  events() {}
+  events() {
+    this.username.addEventListener('keyup', () => {
+      this.isDifferent(this.username, this.usernameHandler);
+    });
+  }
 
   // methods
+  isDifferent(el, handler) {
+    if (el.value !== el.previousValue) {
+      handler.call(this);
+    }
+    el.previousValue = el.value;
+  }
+
+  usernameHandler() {
+    alert('usernameHandler just ran');
+  }
+
   insertValidationElements() {
     this.allFields.forEach(el => {
       el.insertAdjacentHTML(
         'afterend',
-        '<p class="alert alert-danger small liveValidateMessage liveValidateMessage--visible">Validation Message</p>'
+        '<p class="alert alert-danger small liveValidateMessage"></p>'
       );
     });
   }
