d6efca3624bbf08aeb0da6b175c3ca1f6acbf816
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-26 11:48:19 +0100

    Working on validation: init for registration form

---
+++ frontend-js/modules/registrationForm.js
@@ -0,0 +1,12 @@
+export default class RegistrationForm {
+  constructor() {
+    this.events();
+  }
+
+  //events
+  events() {
+    alert('Registration Form js is running');
+  }
+
+  // methods
+}
