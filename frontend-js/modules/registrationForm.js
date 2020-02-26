443aac6ea2954c444f3041998e4bd484de6f1be7
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-26 12:09:58 +0100

    Working on validation: create validation fields and show them for testing

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -1,12 +1,22 @@
 export default class RegistrationForm {
   constructor() {
+    this.allFields = document.querySelectorAll(
+      '#registration-form .form-control'
+    );
+    this.insertValidationElements();
     this.events();
   }
 
   //events
-  events() {
-    alert('Registration Form js is running');
-  }
+  events() {}
 
   // methods
+  insertValidationElements() {
+    this.allFields.forEach(el => {
+      el.insertAdjacentHTML(
+        'afterend',
+        '<p class="alert alert-danger small liveValidateMessage liveValidateMessage--visible">Validation Message</p>'
+      );
+    });
+  }
 }
