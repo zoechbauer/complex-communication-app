76ac40ceaa3c8f28d5a03dbc3a960b0b36db172e
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-27 19:32:04 +0100

    Working on API: config app.js for separate file for api routes - test login with Postman

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const csrf = require('csurf');
 const app = express();
 const sanitizeHTML = require('sanitize-html');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api', require('./router-api'));
+
 let sessionOptions = session({
   secret: 'enter a secret string that others cannot guess easily',
   store: new MongoStore({ client: require('./db') }),
@@ -64,9 +69,6 @@ app.use(function(req, res, next) {
 });
 
 const router = require('./router');
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 app.use(express.static('public'));
 app.set('views', 'views');
