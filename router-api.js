56fea4ecb392ff614de346092c6f4f757d7b9e85
Zoechbauer <hans.zoechbauer@aon.at>
2020-02-28 13:00:05 +0100

    Working on API: configure CORS for using the api from any site

--- router-api.js
+++ router-api.js
@@ -1,7 +1,11 @@
 const apiRouter = require('express').Router();
+
 const userController = require('./controllers/userController');
 const postController = require('./controllers/postController');
 const followController = require('./controllers/followController');
+const cors = require('cors');
+
+apiRouter.use(cors());
 
 apiRouter.post('/login', userController.apiLogin);
 apiRouter.post(
